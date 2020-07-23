5150a52805616a4873876a4d960ad7a293e575c7
Piotr Zagórowicz <piotr.zagorowicz@gmail.com>
2020-07-23 13:35:16 +0200

    node server setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { callApi } from './callApi';
 
 function App() {
+
+  useEffect(() => {
+    callApi('/api/hello')
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ client/src/callApi.ts
@@ -0,0 +1,8 @@
+const callApi = async (url: string) => {
+    const response = await fetch(url);
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+};
+
+export { callApi };
